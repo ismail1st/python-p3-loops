5c8ca50cead7fc6d599dbd8027ce1486bd04d318
Ismail Hassan <ismaylhassan8@gmail.com>
2025-11-18 11:55:07 +0300

    lab

--- lib/looping.py
+++ lib/looping.py
@@ -1,13 +1,26 @@
-#!/usr/bin/env python3
-
 def happy_new_year():
-    # code goes here!
-    pass
+    # Print from 10 down to 1
+    i = 10
+    while i > 0:
+        print(i)
+        i -= 1
+    print("Happy New Year!")
+
 
 def square_integers(int_list):
-    # code goes here!
-    pass
+    squared = []
+    for num in int_list:
+        squared.append(num * num)
+    return squared
+
 
 def fizzbuzz():
-    # code goes here!
-    pass
+    for num in range(1, 101):
+        if num % 3 == 0 and num % 5 == 0:
+            print("FizzBuzz")
+        elif num % 3 == 0:
+            print("Fizz")
+        elif num % 5 == 0:
+            print("Buzz")
+        else:
+            print(num)
